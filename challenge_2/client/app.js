71fc249a1410ad7e123760b1fab07e711461d45e
dyliman <dyyliman@gmail.com>
2018-07-10 15:19:42 -0700

    working but not nice

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,38 @@
+$(document).ready(function(){
 
+	$('.submit').click(function(e){
+	 	e.preventDefault();
+		var info = $('.text').val();
+		var send = {
+			text: info,
+		}
+		$.ajax({
+			url: 'http://127.0.0.1:3000/',
+		    type: 'POST',
+		    data: send,
+		    datatype: 'application/JSON',
+			success: function(data) {
+		      console.log('successful');
+		      console.log(data)
+		      $('.text').val('')
+		      handleData(data);
+		    },
+		    error: function(err) {
+		      console.log('failed');
+		      console.log(err);
+		    }
+		})
+	})
 
+	var handleData = function(data){
+		for(var i = 0; i < data.firstName.length; i++){
+			$('.firstname').append(`<div>${data.firstName[i]}</div>`)
+			$('.lastname').append(`<div>${data.lastName[i]}</div>`)
+			$('.county').append(`<div>${data.county[i]}</div>`)
+			$('.city').append(`<div>${data.city[i]}</div>`)
+			$('.role').append(`<div>${data.role[i]}</div>`)
+			$('.sales').append(`<div>${data.sales[i]}</div>`)
+		}
+	}
+
+})
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+app = express();
 
 
+app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get('/', function(req, res) {
+
+})
+
+app.post('/', function(req, res) {
+	console.log(req.body.text)
+	var body = JSON.parse(req.body.text)
+	var returnObj = {}
+	returnObj.firstName = [];
+	returnObj.lastName = [];
+	returnObj.county = [];
+	returnObj.city = [];
+	returnObj.role = [];
+	returnObj.sales = [];
+
+	var recurObject = function(object){
+		returnObj.firstName.push(object.firstName);
+		returnObj.lastName.push(object.lastName);
+		returnObj.county.push(object.county);
+		returnObj.city.push(object.city);
+		returnObj.role.push(object.role);
+		returnObj.sales.push(object.sales);
+		if(object.children.length > 0){
+			for(var i = 0; i < object.children.length; i++){
+				recurObject(object.children[i])
+			}
+		}
+	}
+	recurObject(body);
+
+    res.status(201).send(returnObj);
+});
+
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+});
