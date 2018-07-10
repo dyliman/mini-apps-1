ee55b221937e2ca6fbdbf236e363fc71d6c7f982
dyliman <dyyliman@gmail.com>
2018-07-09 17:15:03 -0700

    Bitwise

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,89 +1,133 @@
 document.addEventListener('DOMContentLoaded', function() {
 	
-	var currentTurn = true //true=X false=O
+	var Game = function() {
+		this.currentTurn = true //true=X false=O
+		this.reset = document.getElementsByClassName('reset')[0];
+		this.ticTacToe = document.getElementsByClassName('ticTacToe')[0];
+		this.moves = 0
+		this.boardO = new Array(3).fill(0)
+		this.boardX = new Array(3).fill(0)
+		this.fullBoard = [];
+		this.playerOneScore = 0;
+		this.playerTwoScore = 0;
+	}
 
-	var one = document.getElementsByClassName('one')[0];
-	var two = document.getElementsByClassName('two')[0];
-	var three = document.getElementsByClassName('three')[0];
-	var four = document.getElementsByClassName('four')[0];
-	var five = document.getElementsByClassName('five')[0];
-	var six = document.getElementsByClassName('six')[0];
-	var seven = document.getElementsByClassName('seven')[0];
-	var eight = document.getElementsByClassName('eight')[0];
-	var nine = document.getElementsByClassName('nine')[0];
-	var reset = document.getElementsByClassName('reset')[0];
+	Game.prototype.init = function(){
 
-	var items = [one,two,three,four,five,six,seven,eight,nine];
-	var resetItems = ['1','2','3','4','5','6','7','8','9'];
-	var moves = 0	
+		this.ticTacToe.addEventListener('click', (e) => {
+			this.handleClick(e);
+		});
 
-	for(var i = 0; i < items.length; i++)
-	items[i].addEventListener('click', function(e){
-		handleClick(e);
-	});	
+		this.reset.addEventListener('click', (e) => {
+			this.handleReset(e);
+		});
 
-	reset.addEventListener('click', function(e){
-		handleReset(e);
-	})
-   
-	var handleClick = function(event){
-	console.log(event)
-		if(event.target.innerHTML === "X" || event.target.innerHTML === "O"){
-			//do nothing
-		} else {
-			if(currentTurn){
+		this.createFullBoard();
+		
+	}
+
+	Game.prototype.createFullBoard = function(){
+		this.fullBoard = new Array(3);
+
+		for(var i = 0; i < this.boardO.length; i++){
+			this.fullBoard[i] = this.boardO[i] ^ this.boardX[i]
+		}
+	}
+
+	Game.prototype.handleClick = function(event){
+	var value = event.target.attributes[1].value;
+	var elementClassName = event.path[1].className;
+		if(elementClassName === 'row1' && (value & this.fullBoard[0]) == value){
+			//do nothing, checks if move is valid
+		} else if (elementClassName === 'row2' && (value & this.fullBoard[1]) == value){
+			//do nothing, checks if move is valid
+		} else if (elementClassName === 'row3' && (value & this.fullBoard[2]) == value){
+			//do nothing, checks if move is valid
+		} else if(this.currentTurn){
 			event.target.innerHTML = "X";
-			currentTurn = !currentTurn;
-			} else{
-			event.target.innerHTML = "O";
-			currentTurn = !currentTurn;
+			this.currentTurn = !this.currentTurn;
+
+			if(elementClassName === 'row1'){
+				this.boardX[0] = value ^ this.boardX[0]
+			} else if (elementClassName === 'row2'){
+				this.boardX[1] = value ^ this.boardX[1]
+			} else if (elementClassName === 'row3'){
+				this.boardX[2] = value ^ this.boardX[2]
 			}
-			checkBoard();
+			this.checkBoard();
+
+		} else if (!this.currentTurn){
+			event.target.innerHTML = "O";
+			this.currentTurn = !this.currentTurn;
+
+			if(elementClassName === 'row1'){
+				this.boardO[0] = value ^ this.boardO[0]
+			} else if (elementClassName === 'row2'){
+				this.boardO[1] = value ^ this.boardO[1]
+			} else if (elementClassName === 'row3'){
+				this.boardO[2] = value ^ this.boardO[2]
+			}
+			this.checkBoard();
 		}
+
 	}
 
-	var handleReset = function(event){
-		for(var i = 0; i < items.length; i++){
-			items[i].innerHTML = resetItems[i]
+	Game.prototype.handleReset = function(event){
+		this.currentTurn = true
+		this.moves = 0
+		this.boardO = new Array(3).fill(0)
+		this.boardX = new Array(3).fill(0)
+		var elements = document.querySelectorAll('.cell')
+		for(var i = 0; i < elements.length; i++){
+			elements[i].innerHTML = '';
 		}
-		currentTurn = true
-		moves = 0
+		this.init();
 	}
 
-	var checkBoard = function(){
-		if(one.innerHTML === two.innerHTML && two.innerHTML === three.innerHTML){
-			console.log(one.innerHTML)
-			console.log(two.innerHTML)
-			console.log(three.innerHTML)
-			alert(one.innerHTML + "WINS!")
-		}
-		if(four.innerHTML === five.innerHTML && five.innerHTML === six.innerHTML){
-			alert(four.innerHTML + "WINS!")
-		}
-		if(seven.innerHTML === eight.innerHTML && eight.innerHTML === nine.innerHTML){
-			alert(seven.innerHTML + "WINS!")
-		}
-		if(one.innerHTML === five.innerHTML && five.innerHTML === nine.innerHTML){
-			alert(one.innerHTML + "WINS!")
-		}
-		if(three.innerHTML === five.innerHTML && five.innerHTML === seven.innerHTML){
-			alert(three.innerHTML + "WINS!")
-		}
-		if(one.innerHTML === four.innerHTML && four.innerHTML === seven.innerHTML){
-			alert(one.innerHTML + "WINS!")
-		}
-		if(two.innerHTML === five.innerHTML && five.innerHTML === eight.innerHTML){
-			alert(two.innerHTML + "WINS!")
-		}
-		if(three.innerHTML === six.innerHTML && six.innerHTML === nine.innerHTML){
-			alert(three.innerHTML + "WINS!")
-		}
+	Game.prototype.checkBoard = function(){
+		this.createFullBoard();
+		this.moves += 1;
 
-		moves += 1;
-		if(moves === 9){
+		if(this.boardO.includes(7)){
+			this.playerTwoScore += 1
+			this.handleWin(this.playerTwo)
+		} else if(this.boardX.includes(7)){
+			this.playerOneScore += 1
+			this.handleWin(this.playerOne)
+		} else if((this.boardO[0] & this.boardO[1] & this.boardO[2]) !== 0){
+			this.playerTwoScore += 1
+			this.handleWin(this.playerTwo)
+		} else if((this.boardX[0] & this.boardX[1] & this.boardX[2]) !== 0){
+			this.playerOneScore += 1
+			this.handleWin(this.playerOne)
+		} else if((this.boardO[0] >> 2 & this.boardO[1] >> 1 & this.boardO[2]) == 1 || (this.boardO[0] & this.boardO[1] >> 1 & this.boardO[2] >> 2) == 1){
+			this.playerTwoScore += 1
+			this.handleWin(this.playerTwo)
+		} else if((this.boardX[0] >> 2 & this.boardX[1] >> 1 & this.boardX[2]) == 1 || (this.boardX[0] & this.boardX[1] >> 1 & this.boardX[2] >> 2) == 1){
+			this.playerOneScore += 1
+			this.handleWin(this.playerOne)
+		} else if(this.moves === 9){
 			alert("TIE!")
 		}
 	}
+
+	Game.prototype.handleWin = function(winner){
+		setTimeout(() => {alert(`The Winner Is: ${winner}!!`)} , 10)
+		document.getElementsByClassName('player1')[0].innerHTML = `${this.playerOne} X: ${this.playerOneScore}`
+		document.getElementsByClassName('player2')[0].innerHTML = `${this.playerTwo} O: ${this.playerTwoScore}`
+	}
+
+	Game.prototype.players = function(){
+		this.playerOne = prompt('Player One\'s Name (X)') || 'Player One'
+		this.playerTwo = prompt('Player Two\'s Name (O)') || 'Player Two'
+
+		document.getElementsByClassName('player1')[0].innerHTML = `${this.playerOne} X: ${this.playerOneScore}`
+		document.getElementsByClassName('player2')[0].innerHTML = `${this.playerTwo} O: ${this.playerTwoScore}`
+	}
+
+currentGame = new Game;
+currentGame.init();
+currentGame.players();
 
 });
 
